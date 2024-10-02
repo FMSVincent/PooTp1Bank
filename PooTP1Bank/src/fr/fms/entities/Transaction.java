e25429e51591a92efd39f86a20da29f2f9e95374
Ozmanov Alik <alik.ozmanov@fms-ea.com>
2024-10-02 14:05:54 +0200

    Transaction

---
+++ PooTP1Bank/src/fr/fms/entities/Transaction.java
@@ -0,0 +1,60 @@
+package fr.fms.entities;
+
+public class Transaction {
+	// Attributs
+	private int idTransaction;
+	private double amount;
+	private String date;
+	private String transactionType;
+	
+	// Constructor
+	public Transaction(int idTransaction,double amount,String date,String transactionType) {
+		this.idTransaction = idTransaction;
+		this.amount = amount;
+		this.date = date;
+		this.transactionType = transactionType;
+	}
+
+	public int getIdTransaction() {
+		return idTransaction;
+	}
+
+	public void setIdTransaction(int idTransaction) {
+		this.idTransaction = idTransaction;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	
+	public void consult() {
+		System.out.println("Transaction Id :" + idTransaction );
+		System.out.println("Amount :" + amount );
+		System.out.println("Date :" + date );
+		System.out.println("Transaction Type :" + transactionType );
+	}
+
+	
+	
+
+}
